80cfb4709c17eda57b850b2c80219d513b131eaa
Michal Tuček <michaltk1@gmail.com>
2020-12-02 22:22:47 +0100

    Added registration logic to AuthController & minor edit in register view

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Models\RegisterModel;
 use Okami\Core\Controller;
 use Okami\Core\Request;
 
@@ -24,10 +25,20 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
+        $registerModel = new RegisterModel();
         if ($request->isPost()) {
-            return "Handling submitted data...";
+            $registerModel->loadData($request->getBody());
+
+            if ($registerModel->validate() && $registerModel->register()) {
+                return 'Success';
+            }
+            return $this->render('register', [
+                'model' => $registerModel
+            ]);
         }
         $this->setLayout('auth');
-        return $this->render('register');
+        return $this->render('register', [
+            'model' => $registerModel
+        ]);
     }
 }
