c3d588b0b02642c558a5446fcfe04cbdbd214fd1
Michal Tuček <michaltk1@gmail.com>
2020-12-05 22:06:43 +0100

    Edited basic migrations

--- migrations/m0001_createUserTable.php
+++ migrations/m0001_createUserTable.php
@@ -8,8 +8,8 @@ class m0001_createUserTable {
             CREATE TABLE users (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 email VARCHAR(255) NOT NULL,
-                firstname VARCHAR(255) NOT NULL,
-                lastname VARCHAR(255) NOT NULL,
+                firstName VARCHAR(255) NOT NULL,
+                lastName VARCHAR(255) NOT NULL,
                 status TINYINT NOT NULL,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             ) ENGINE=INNODB;";
--- migrations/m0002_addPasswordColumnToUserTable.php
+++ migrations/m0002_addPasswordColumnToUserTable.php
@@ -4,7 +4,7 @@ class m0002_addPasswordColumnToUserTable {
     public function up()
     {
         $db = \Okami\Core\App::$app->db;
-        $db->pdo->exec("ALTER TABLE users ADD COLUMN password VARCHAR(512) NOT NULL;");
+        $db->pdo->exec("ALTER TABLE users ADD COLUMN password VARCHAR(512) NOT NULL AFTER email;");
     }
 
     public function down()
