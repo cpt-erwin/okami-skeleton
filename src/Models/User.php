5e1a9bf50c1788abeb37848c9f33d896970aceb6
Michal Tuček <michaltk1@gmail.com>
2020-12-07 16:31:40 +0100

    Implemented primary key setting method

--- src/Models/User.php
+++ src/Models/User.php
@@ -33,6 +33,11 @@ class User extends DbModel
         return 'user';
     }
 
+    public function primaryKey(): string
+    {
+        return 'id';
+    }
+
     public function save()
     {
         $this->password = password_hash($this->password, PASSWORD_DEFAULT);
