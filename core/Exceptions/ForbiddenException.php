d7953d8fcd30f4bb1bec21dc581bb71f344a3b03
Michal Tuček <michaltk1@gmail.com>
2021-01-25 11:28:02 +0100

    Added custom ForbiddenException

---
+++ core/Exceptions/ForbiddenException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Okami\Core\Exceptions;
+
+use Exception;
+
+class ForbiddenException extends Exception
+{
+    protected $message = 'You don\'t have permission to access this page!';
+    protected $code = 403;
+}
